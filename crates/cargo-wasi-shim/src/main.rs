84c725d6aecc5073edec29b18e0b8b26ee1a0c1b
Alex Crichton <alex@alexcrichton.com>
2019-10-23 10:10:22 -0700

    Fill out the shim in its hopefully final-ish form

--- crates/cargo-wasi-shim/src/main.rs
+++ crates/cargo-wasi-shim/src/main.rs
@@ -1,3 +1,8 @@
+use std::path::PathBuf;
+use std::io::{self, Write};
+use std::process::Command;
+use std::fs::OpenOptions;
+
 cfg_if::cfg_if! {
     if #[cfg(feature = "locally-developed")] {
         const BYTES: Option<&[u8]> = Some(include_bytes!(env!("BYTES_LOC")));
@@ -13,8 +18,64 @@ cfg_if::cfg_if! {
 }
 
 fn main() {
-    match BYTES {
-        Some(n) => println!("got {} bytes", n.len()),
-        None => println!("no bytes"),
+    let bytes = match BYTES {
+        Some(n) => n,
+        None => panic!("unsupported fallback platform"),
+    };
+    let mut args = std::env::args_os();
+    let me = match args.next() {
+        Some(name) => name,
+        None => std::process::exit(1),
+    };
+
+    let path = PathBuf::from(me);
+    let file_name = match path.file_name().and_then(|f| f.to_str()) {
+        Some(s) => format!(".{}", s),
+        None => std::process::exit(2),
+    };
+    let candidate = path.with_file_name(file_name);
+
+    let mut cmd = Command::new(&candidate);
+    for arg in args {
+        cmd.arg(arg);
     }
+    match &exec(&mut cmd) {
+        Ok(()) => return,
+        Err(e) if e.kind() == io::ErrorKind::NotFound => {}
+        Err(e) => {
+            eprintln!("failed to spawn child process: {}", e);
+            std::process::exit(3);
+        }
+    }
+
+    let mut opts = OpenOptions::new();
+    opts.create(true).write(true);
+    #[cfg(unix)]
+    {
+        use std::os::unix::prelude::*;
+        opts.mode(0o755);
+    }
+    if let Err(e) = opts.open(&candidate).and_then(|mut f| f.write_all(bytes)) {
+        eprintln!("failed to write executable file `{}`: {}", candidate.display(), e);
+        std::process::exit(4);
+    }
+    match exec(&mut cmd) {
+        Ok(()) => return,
+        Err(e) => {
+            eprintln!("failed to spawn child process: {}", e);
+            std::process::exit(5);
+        }
+    }
+}
+
+#[cfg(unix)]
+fn exec(cmd: &mut Command) -> io::Result<()> {
+    use std::os::unix::prelude::*;
+    Err(cmd.exec())
+}
+
+#[cfg(windows)]
+fn exec(cmd: &mut Command) -> io::Result<()> {
+    let status = cmd.status()?;
+    std::process::exit(status.code().unwrap_or(6));
 }
---
+++ crates/cargo-wasi-shim/tests/test.rs
@@ -0,0 +1,47 @@
+use std::path::PathBuf;
+use std::process::Command;
+use std::fs;
+
+fn me() -> PathBuf {
+    let mut me = std::env::current_exe().unwrap();
+    me.pop();
+    me.pop();
+    me.push("cargo-wasi-shim");
+    me.set_extension(std::env::consts::EXE_EXTENSION);
+    return me;
+}
+
+fn case() -> (tempfile::TempDir, PathBuf) {
+    let td = tempfile::TempDir::new().unwrap();
+    let path = td.path().join("cargo-wasi");
+    fs::copy(me(), &path).unwrap();
+    (td, path)
+}
+
+#[test]
+fn smoke() {
+    let (_td, path) = case();
+    let output = Command::new(&path).output().unwrap();
+    println!("{:#?}", output);
+    assert!(output.status.success());
+    assert!(path.with_file_name(".cargo-wasi").exists());
+}
+
+#[test]
+fn pass_args() {
+    let (_td, path) = case();
+    let output = Command::new(&path).arg("--help").output().unwrap();
+    println!("{:#?}", output);
+    assert!(output.status.success());
+}
+
+#[test]
+fn run_twice() {
+    let (_td, path) = case();
+    let output = Command::new(&path).output().unwrap();
+    println!("{:#?}", output);
+    assert!(output.status.success());
+    let output = Command::new(&path).output().unwrap();
+    println!("{:#?}", output);
+    assert!(output.status.success());
+}
