b11527d2428eee95af4128239a8ed732a0bfdd3a
Alex Crichton <alex@alexcrichton.com>
2020-01-06 08:04:11 -0800

    Relax a test assertion

--- tests/tests/main.rs
+++ tests/tests/main.rs
@@ -572,7 +572,7 @@ test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out
         .stderr(is_match(
             "^\
 .*Compiling foo v1.0.0 .*
-.*Finished dev .*
+.*Finished .*
 .*Running .*
 .*Running `.*`
 $",
